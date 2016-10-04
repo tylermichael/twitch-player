bd44e5c61a22213665f6a0179dd66eba0d1dd36e
Tyler Michael <tmich214@gmail.com>
2016-10-04 01:44:32 -0400

    add logic for top streams

--- src/stores/StreamStore.js
+++ src/stores/StreamStore.js
@@ -6,6 +6,7 @@ import Stream from '../models/Stream';
 class StreamStore {
 	@observable followed = [];
 	@observable games = [];
+	@observable topStreams = [];
 	APIService;
 	UIStore;
 
@@ -19,6 +20,7 @@ class StreamStore {
 		if(TwitchAPI.authenticated) {
 			this.getFollowed();
 			this.getGames();
+			this.getTopStreams();
 		}
     this.UIStore.isDoneLoading = true;
 	}
@@ -40,7 +42,7 @@ class StreamStore {
 	getTopStreams(offset: Number = 0, replace: Boolean = false) {
 		TwitchAPI.getTopStreams()
 		.then(result => {
-			this.games = result.data.top;
+			this.topStreams = result.data.streams;
 		})
 	}
 }
