027d6e6e6c73566105134a30cf14f77c47be3016
Tyler Michael <tmich214@gmail.com>
2016-10-06 01:15:25 -0400

    remove junk json

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -106,33 +106,3 @@ class ListItem extends Component {
 }
 
 export default ListItem;
-
-
-
-
-
-
-
-// {
-//   "game": {
-//     "name": "League of Legends",
-//     "popularity": 77120,
-//     "_id": 21779,
-//     "giantbomb_id": 24024,
-//     "box": {
-//       "large": "https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-272x380.jpg",
-//       "medium": "https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-136x190.jpg",
-//       "small": "https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-52x72.jpg",
-//       "template": "https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-{width}x{height}.jpg"
-//     },
-//     "logo": {
-//       "large": "https://static-cdn.jtvnw.net/ttv-logoart/League%20of%20Legends-240x144.jpg",
-//       "medium": "https://static-cdn.jtvnw.net/ttv-logoart/League%20of%20Legends-120x72.jpg",
-//       "small": "https://static-cdn.jtvnw.net/ttv-logoart/League%20of%20Legends-60x36.jpg",
-//       "template": "https://static-cdn.jtvnw.net/ttv-logoart/League%20of%20Legends-{width}x{height}.jpg"
-//     },
-//     "_links": {}
-//   },
-//   "viewers": 77409,
-//   "channels": 1280
-// }
