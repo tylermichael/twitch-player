36ecf5ad5f398d09b32ab6cd7fe2ee681a92c8fa
Tyler Michael <tmich214@gmail.com>
2016-10-09 02:01:52 -0400

    add logic to TabBody for multiple content bodes

--- src/components/Tab/TabBody.js
+++ src/components/Tab/TabBody.js
@@ -1,20 +1,44 @@
 /* @flow */
 
 import React, { Component } from 'react';
+import { observer } from 'mobx-react';
 import classnames from 'classnames';
 
+import { ListItem } from '../List';
+
+@observer
 class TabBody extends Component {
 	render(): any {
 
-    let { children, selected } = this.props;
+    let { children, selected, UIStore, StreamStore, type } = this.props;
+		let channels;
     let container_list_props = {
       className: classnames({
         'channel-list-container__list': true,
-        'channel-list-container__list--selected': selected
+        'channel-list-container__list--selected': selected,
+				'channel-list-container__list--secondary': UIStore.secondaryContent === 'games' && type === 'category'
       })
     };
+		if(type === 'category') {
+			channels = StreamStore.followed.map((stream: Object, index: Number): any =>
+	      <ListItem
+	        key={stream._id}
+	        type="stream"
+	        stream={stream}
+	      />
+	    );
+		}
 
-		return <div {...container_list_props}>{children}</div>
+		return <div {...container_list_props}>
+			<div className="channel-list-container__wrapper">
+				<div className="channel-list-container__list__column channel-list-container__list-primary">
+						{children}
+					</div>
+				<div className='channel-list-container__list__column channel-list-container__list-secondary'>
+					{channels}
+				</div>
+			</div>
+		</div>
 	}
 }
 
