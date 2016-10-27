acdf7ff5e54eec4741ae541a9b676edd94b5f6cc
Tyler Michael <tmich214@gmail.com>
2016-10-27 17:56:29 -0400

    make prod build do everything it needs to

--- config/babel.prod.js
+++ config/babel.prod.js
@@ -1,10 +1,12 @@
 module.exports = {
   presets: [
+    'babel-preset-stage-0',
     'babel-preset-es2015',
     'babel-preset-es2016',
     'babel-preset-react'
   ].map(require.resolve),
   plugins: [
+    'babel-plugin-transform-decorators-legacy',
     'babel-plugin-syntax-trailing-function-commas',
     'babel-plugin-transform-class-properties',
     'babel-plugin-transform-object-rest-spread',
